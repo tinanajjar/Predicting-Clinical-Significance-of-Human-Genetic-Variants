27b28b390a171931b5b35be703060c9ecd5a2f7e
Tina Alnajjar <tina.alnajjar@icloud.com>
2026-04-04 20:48:35 +0300

    Implement data ingestion from TSV to MongoDB
    Add script to ingest data from TSV file into MongoDB.

--- ingestion.py
+++ ingestion.py
@@ -1,1 +1,23 @@
+# ingestion.py
+import csv
+from pymongo import MongoClient
 
+client = MongoClient("mongodb://localhost:27017/")
+db = client["clinvarDB"]
+collection = db["variants"]
+
+with open("Dataset/file.tsv", "r") as file:
+    reader = csv.DictReader(file, delimiter="\t")
+    
+    batch = []
+    for i, row in enumerate(reader):
+        batch.append(row)
+        
+        if len(batch) == 5000:
+            collection.insert_many(batch)
+            batch = []
+
+    if batch:
+        collection.insert_many(batch)
+
+print("Data inserted successfully!")
