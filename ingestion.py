d81ce5b1455da051d13b20844fd83adc6fcec198
Tina Alnajjar <tina.alnajjar@icloud.com>
2026-04-04 20:50:13 +0300

    Refactor ingestion script for new database structure
    Updated database and collection names, removed batch insertion for TSV file, and added a single document insertion.

--- ingestion.py
+++ ingestion.py
@@ -1,23 +1,10 @@
-# ingestion.py
-import csv
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017/")
-db = client["clinvarDB"]
-collection = db["variants"]
+print("Connected successfully")
 
-with open("Dataset/file.tsv", "r") as file:
-    reader = csv.DictReader(file, delimiter="\t")
-    
-    batch = []
-    for i, row in enumerate(reader):
-        batch.append(row)
-        
-        if len(batch) == 5000:
-            collection.insert_many(batch)
-            batch = []
+db = client["test_db"]
+collection = db["test_collection"]
 
-    if batch:
-        collection.insert_many(batch)
-
-print("Data inserted successfully!")
+collection.insert_one({"name": "Ahmad", "status": "working"})
+print("Inserted successfully")
