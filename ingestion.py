440148125dbf609e2bc1e4fb74e5167fd5cf81be
Tina Alnajjar <tina.alnajjar@icloud.com>
2026-04-05 22:15:01 +0300

    Update ingestion.py

--- ingestion.py
+++ ingestion.py
@@ -3,6 +3,12 @@ import csv
 import gzip
 from pymongo import MongoClient
 
+
+#Part 1 — Ingestion
+#Reads the dataset file
+#Converts each row into a MongoDB document
+#Inserts data in batches (5000 rows at a time)
+
 client = MongoClient("mongodb://localhost:27017/")
 print("Connected successfully")
 
@@ -23,7 +29,7 @@ with gzip.open(r"C:\Users\HP\Desktop\big data project\variant_summary.txt.gz", "
 
         if len(batch) >= batch_size:
             collection.insert_many(batch)
-            print(f"Inserted {len(batch)} records")
+            #print(f"Inserted {len(batch)} records")
             batch = []
 
     if batch:
@@ -31,3 +37,22 @@ with gzip.open(r"C:\Users\HP\Desktop\big data project\variant_summary.txt.gz", "
         print(f"Inserted final {len(batch)} records")
 
 print("Data loading completed")
+
+# Aggregation pipeline:
+# Group data by "Type" and count how many records per type
+pipeline = [
+    {"$group" : {
+        "_id" : "$Type", # group by Type column
+        "count" : {"$sum" : 1} # count number of rows in each group
+    }
+    },
+    {"$sort" : {"count" : -1} # sort results from highest to lowest count
+    }
+]
+
+# Run the aggregation query
+results = collection.aggregate(pipeline)
+
+# Print the results
+for doc in results:
+    print(doc)
